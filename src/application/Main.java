0f9411e7a7771553b233e8d66a848ffea2eee254
Vineee75 <viniciussouza81@yahoo.com.br>
2020-06-06 11:31:40 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +21,7 @@ public class Main extends Application {
 			sp.setFitToHeight(true);
 			sp.setFitToWidth(true);
 
-			Scene mainScene = new Scene(sp);
+			mainScene = new Scene(sp);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,4 +29,13 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 }
